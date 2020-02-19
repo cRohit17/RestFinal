3366977333a031eb57a1c0593b7cdaeb0d721f58
rohit <rohit.chinchkar@calsoftinc.com>
2020-02-19 17:59:05 +0530

    5

--- TestRest/src/main/java/com/spring/rest/util/SessionFactoryUtil.java
+++ TestRest/src/main/java/com/spring/rest/util/SessionFactoryUtil.java
@@ -9,6 +9,8 @@ public class SessionFactoryUtil
 	// My comment 3 here.
 	// My comment 2 here.
 	// My comment 4 here.
+		// My comment 5 here.
+	
 	public static void closeResources(Session session,Transaction tr){
 		try{
 			if(session!=null){
